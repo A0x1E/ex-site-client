d4ff45a70b8451e422cad2a59e7073eddf3801aa
Sergey Maslakov <s.a.maslakov@gmail.com>
2022-02-19 14:31:59 +0000

    chore!: add the template code

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import { presetWind } from 'unocss'
+
+const config: UserConfig = {
+         plugins: [vue(), Unocss({presets: [presetWind()]})]
+}
+
+export default config
